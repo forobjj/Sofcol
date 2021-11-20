c4a05822c59a201e9b1ec4dae0f8b36ec6a42462
Jonathan Echeverri <87216359+forobjj@users.noreply.github.com>
2021-11-20 15:28:11 -0500

    Se sube el CRUD de la tabal usuarios

---
+++ usuarios.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+
+const url = 'mongodb+srv://user:SofCol.C4@cluster0.27qgu.mongodb.net/Gestion-de-proyectos?retryWrites=true&w=majority'
+
+//cone
+
+mongoose.connect(url)
+.then(()=>console.log('La conexion esta establecida') )
+.catch( (e)=> console.log("Error en la conexion", + e) )
+
+
+//Schema
+const usuariosSchema = mongoose.Schema({
+    nombre: {
+        type: String,
+        require : true
+    },
+    apellido: {
+        type: String,
+        require : true
+    },
+    cargo: String,
+    correo: String,
+    _cedula :{
+        type: String,
+        require: true,
+        
+    },
+    
+
+});
+//model
+
+const UsuarioModel = mongoose.model('Usuarios', usuariosSchema)
+
+
+//read
+const readElements = async ()=>{
+    const usuarios = await UsuarioModel.find()
+    console.log(usuarios)
+}
+const readElementsByCedula = async (cedula)=>{
+    const usuarios = await UsuarioModel.findById(
+        {_cedula: cedula}
+    )
+    console.log(usuarios)
+}
+
+
+//create
+
+const createUsuario = async ()=>{
+    const usuario = new UsuarioModel({
+        nombre: "Jonathan",
+        apellido: "Echeverri",
+        cargo: "Administrador",
+        correo:"jonathan@gmail.com",
+        _cedula:"1036944735",
+    })
+    const insertUsuario = await usuario.save();
+    console.log(insertUsuario);
+}
+
+
+//update
+const updateUsuario = async (cedula) =>{
+    const usuario = await UsuarioModel.updateOne(
+        {_cedula:cedula},
+        {$set: {
+            apellido: "Arbelaez",
+            correo:"jonathan.echeverri1@gmail.com"
+        }}
+    ) 
+}
+//delete
+ const deleteUsuario = async (cedula)=>{
+     const usuario = await UsuarioModel.deleteOne({cedula: cedula})
+     console.log(usuario)
+ }
+
+
+
+//createUsuario();
+//updateUsuario('1036944735');
+//readElements();
+//deleteUsuario('1036944735');
